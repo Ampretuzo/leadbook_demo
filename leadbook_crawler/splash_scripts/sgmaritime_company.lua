79d169d5391e52e19f63a8b3e384d3774973b1dd
Sergi Chalauri <sergi.chalauri@gmail.com>
2019-03-27 15:31:24 +0400

    Blocking tracking and ad ajax requests for splash

--- leadbook_crawler/splash_scripts/sgmaritime_company.lua
+++ leadbook_crawler/splash_scripts/sgmaritime_company.lua
@@ -1,4 +1,23 @@
+function should_abort(url)
+  return string.find(url, "/tracking/events")
+          or string.find(url, '/log.aspx?')
+          or string.find(url, '/logging/messages')
+          or string.find(url, 'www.google.com/pagead/')
+          or string.find(url, 'googleads.g.doubleclick.net/')
+          or string.find(url, 'www.googleadservices.com/')
+          or string.find(url, 'google-analytics.com/')
+          or string.find(url, 'stats.g.doubleclick.net/')
+          or string.find(url, 'adservice.google.com/')
+end
+
 function main(splash, args)
+  local aborted_requests = {}
+  splash:on_request(function (request)
+      if should_abort(request.url) then
+          table.insert(aborted_requests, request.url)
+          request.abort()
+      end
+  end)
   assert(splash:go(args.url))
   assert(splash:wait(0.5))
   splash:runjs([[
@@ -12,5 +31,6 @@ function main(splash, args)
     -- png = splash:png{render_all=true},
     -- har = splash:har(),
     html = splash:html(),
+    aborted_requests = aborted_requests
   }
 end
